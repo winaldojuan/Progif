1f7ff327997cc8ee278f40d029faca795ccd4847
winaldojuan <winaldojuan@gmail.com>
2017-11-20 13:43:32 +0700

    Fungsi tambahan dan Interface
    Menambahkan fungsi untuk update dan delete.. Menambah interface sederhana untuk mencoba web service. Testing.html memuat interface untuk post, up dan update  memuat interface untuk update

--- perpus.go
+++ perpus.go
@@ -17,10 +17,31 @@ type myBuku struct {
     Tahun_terbit int
     Penerbit string
 }
+type myBuku2 struct{
+	ID_buku int `json:"ID_buku, omitempty"`
+	Judul string `json:"Judul, omitempty"`
+	Penulis string `json:"Penulis, omitempty"`
+	Tahun_terbit int `json:"Tahun_terbit, omitempty"`
+	Penerbit string `json:"Penerbit, omitempty"`
+}
+
 
 
 func main(){
     port:=8181
+    http.HandleFunc("/test", func(w http.ResponseWriter, r*http.Request){
+        http.ServeFile(w,r,"testing.html")
+    })
+    http.HandleFunc("/update/", func (w http.ResponseWriter, r *http.Request){
+        http.ServeFile(w,r,"up.html")
+    })
+
+  http.HandleFunc("/put/", func(w http.ResponseWriter, r *http.Request){
+     
+                http.ServeFile(w,r,"update.html")
+       
+    })
+  
     http.HandleFunc("/buku/", func(w http.ResponseWriter, r*http.Request){
         switch r.Method{
 		case "GET":
@@ -38,8 +59,13 @@ func main(){
 			}
             
         case "POST":
-            //InsertBuku(w,r)
-        case "DELETE":
+            InsertBuku(w,r)
+		case "DELETE":
+			s := r.URL.Path[len("/buku/"):]
+			DeleteBuku(w,r,s)
+		case "PUT":
+			s := r.URL.Path[len("/buku/"):]
+			UpdateBuku(w,r,s)
         default:
             http.Error(w, "Invalid request method.", 405)
         }
@@ -168,4 +194,61 @@ func SearchByTahun(w http.ResponseWriter, r *http.Request, s string){
         json.NewEncoder(w).Encode(&buku)
     }
 	err = rows.Err()
+}
+
+func InsertBuku (w http.ResponseWriter, r *http.Request) {
+	var buku myBuku2
+	dec := json.NewDecoder(r.Body)
+	err:=dec.Decode(&buku)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	db,err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/perpustakaan")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	stmt, err := db.Prepare("INSERT INTO test (ID_buku,Judul, Penulis,Tahun_terbit, Penerbit) VALUES (?,?,?,?,?)")
+	if err != nil{
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(buku.ID_buku, buku.Judul, buku.Penulis, buku.Tahun_terbit, buku.Penerbit)
+}
+
+func DeleteBuku(w http.ResponseWriter, r *http.Request, id string){
+	idbuku,_ := strconv.Atoi(id)
+
+	db,err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/perpustakaan")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	rows,err := db.Query("DELETE FROM test WHERE ID_buku=?",idbuku)
+
+	defer rows.Close()
+
+}
+
+func UpdateBuku (w http.ResponseWriter, r *http.Request, id string) {
+	var buku myBuku2
+	idbuku,_ := strconv.Atoi(id)
+	dec := json.NewDecoder(r.Body)
+	err:=dec.Decode(&buku)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	db,err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/perpustakaan")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	stmt, err := db.Prepare("UPDATE test SET ID_buku = ?,Judul = ?, Penulis = ?,Tahun_terbit =?, Penerbit=? where ID_buku=?")
+	if err != nil{
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(buku.ID_buku, buku.Judul, buku.Penulis, buku.Tahun_terbit, buku.Penerbit, idbuku)
 }
